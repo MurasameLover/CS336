b47288336db25160c7a12aeb31ab67ef8407d894
wang-jinkun <3495333143@qq.com>
2026-06-04 15:50:49 +0800

    feat: implement cross_entropy loss and training utilities

--- assignment1-basics-main/assignment1-basics-main/cs336_basics/cross_entropy.py
+++ assignment1-basics-main/assignment1-basics-main/cs336_basics/cross_entropy.py
@@ -1,2 +1,13 @@
 import torch
 
+def cross_entropy_loss(
+    logits: torch.Tensor,
+    targets: torch.Tensor,
+) -> torch.Tensor:
+    batch_size = logits.shape[0]
+    x_max = logits.max(dim=-1, keepdim=True).values
+    log_sum_exp = x_max.squeeze(-1) + torch.log(
+        torch.exp(logits - x_max).sum(dim=-1)
+    )
+    loss = -logits[range(batch_size), targets] + log_sum_exp
+    return loss.mean()
